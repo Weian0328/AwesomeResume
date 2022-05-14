4fa7fdbb6fa8295a9957a2577788eb5b3266846f
weian <kevin19960328@gmail.com>
2022-05-14 14:57:05 +0800

    add flatpickr

---
+++ app/javascript/controllers/date_controller.js
@@ -0,0 +1,14 @@
+// import { Controller } from "stimulus"
+// import flatpickr from "flatpickr"
+// import "flatpickr/dist/flatpickr.css"
+
+// export default class extends Controller {
+//   connect() {
+//     flatpickr(this.element, {
+//       enableTime: true,
+//       noCalendar: true,
+//       dateFormat: "H:i",
+//       defaultDate: "13:45",
+//     })
+//   }
+// }
--- config/application.rb
+++ config/application.rb
@@ -21,7 +21,6 @@ require 'action_cable/engine'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-
 module MyResume
   class Application < Rails::Application
     config.load_defaults 6.1
